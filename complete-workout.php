3c2366c6cbf82448a50c5260fa9d7d0934b2e7c9
melvinjwilkerson <melvinjwilkerson@gmail.com>
2021-10-28 23:50:27 -0400

    Started the implementation of complete-workout

---
+++ complete-workout.php
@@ -0,0 +1,125 @@
+<?php 
+session_start();
+require_once('config.php');
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+$getAllUserWorkoutsSQL = "SELECT workoutName, numExercises, workoutDate, workoutID FROM workouts WHERE userID = {$_SESSION['id']}";
+$getAllUserWorkoutsPrep = $pdo->prepare($getAllUserWorkoutsSQL);
+$getAllUserWorkoutsPrep->execute();
+$allUserWorkouts = $getAllUserWorkoutsPrep->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($allUserWorkouts);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Submit Exercise Data</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <div class="wrapper">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label for="workouts">Select Workout To Input Data For:</label>
+                <select class="form-control" name="workoutName">
+                <?php
+                    if(!empty($allUserWorkouts)) {
+                        for($i=0;$i<count($allUserWorkouts);$i++) {
+                            $tmpWorkoutDate = $allUserWorkouts[$i]['workoutDate'];
+                            echo "<option value=\"{$allUserWorkouts[$i]['workoutName']}\">{$allUserWorkouts[$i]['workoutDate']}</option>";
+                        }
+                    }
+                ?>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary mb-2" name="getExerciseFields">Next</button>
+        </form>
+        
+        <?php 
+            if(isset($_POST['getExerciseFields'])) {
+                $workoutName = $_POST['workoutName'];
+                $_SESSION['workoutName'] = $workoutName;
+            
+
+            $getAllExerciseNamesOfWorkoutSQL = "SELECT e.name, wd.exID FROM workout_details wd INNER JOIN exercises e ON wd.exID = e.exID INNER JOIN workouts w ON w.workoutID = wd.workoutID WHERE w.workoutName='$workoutName'";
+            $getAllExerciseNamesOfWorkoutPrep = $pdo->prepare($getAllExerciseNamesOfWorkoutSQL);
+            $getAllExerciseNamesOfWorkoutPrep->execute();
+            $exerciseArray = $getAllExerciseNamesOfWorkoutPrep->fetchAll(PDO::FETCH_ASSOC);
+            $_SESSION['exerciseArray'] = $exerciseArray;
+
+            for($i=1; $i<=count($exerciseArray);$i++) {
+                echo "<form action=\"/fitness-app/php-fitness-app/complete-workout.php\" method=\"post\">
+                    <div class=\"form-group\">
+                    <label for=\"exercise{$i}sets\">Exercise {$i} Sets</label>
+                    <select class=\"form-control\" name=\"exercise{$i}sets\">"; 
+
+                    for($j=1;$j<=10;$j++) {
+                        echo "<option>{$j}</option>";
+                    }
+
+                    echo "</select>
+                    </div>";
+            }
+            echo "<button type=\"submit\" class=\"btn btn-primary mb-2\" name=\"submitSets\">Generate Reps and Weight Fields</button>";
+                echo "</form>";
+            
+            }
+
+            if(isset($_POST['submitSets'])) {
+                for($i=1;$i<=count($_SESSION['exerciseArray']);$i++) {
+                    echo "<form action=\"/fitness-app/php-fitness-app/complete-workout.php\" method=\"post\"><div class=\"form-group\">";
+                    for($j=1;$j<=$_POST["exercise{$i}sets"];$j++) {
+                        echo "<div class=\"mb-3\"><label class=\"form-label\">"; echo $_SESSION['exerciseArray'][$i-1]['name']; echo " Set {$j} Reps"; echo "</label>";
+                        echo "<input type=\"number\" class=\"form-control\" name=\""; echo "ex{$i}set{$j}reps"; echo"\"></div>";
+                        echo "<div class=\"mb-1\"><label class=\"form-label\">"; echo $_SESSION['exerciseArray'][$i-1]['name']; echo " Set {$j} Weight"; echo "</label>";
+                        echo "<input type=\"number\" class=\"form-control\" name=\""; echo "ex{$i}set{$j}weight"; echo"\"></div>";
+                    }
+                }
+                echo "</div><button type=\"submit\" class=\"btn btn-primary mb-2\" name=\"submitWeightAndReps\">Add Completed Workout to the Database</button></form>";
+            }
+
+            if(isset($_POST['submitWeightAndReps'])) {
+                $fields = array();
+                $values = array();
+                foreach($_POST as $field => $value) {
+                    $fields[] = $field;
+                    $values[] = $value;
+                }
+
+                //print_r($_SESSION['exerciseArray']);
+
+                $userID = $_SESSION['id'];
+                $workoutName = $_SESSION['workoutName'];
+
+                $getWorkoutID = "SELECT workoutID from workouts WHERE workoutName='$workoutName' AND userID=$userID";
+                $getWorkoutIDPrep = $pdo->prepare($getWorkoutID);
+                $getWorkoutIDPrep->execute();
+                $workoutIDarr = $getWorkoutIDPrep->fetch(PDO::FETCH_ASSOC);
+                $workoutID = $workoutIDarr['workoutID'];
+
+                print_r($fields);
+                print_r($values);
+
+                
+
+            }
+        ?>
+    </div>
+    
+</body>
+</html>
