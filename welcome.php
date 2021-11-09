bd90e856caaf647811b22e4e25020cdc18773dcf
melvinjwilkerson <melvinjwilkerson@gmail.com>
2021-11-09 05:57:32 -0500

    Added a navbar.

--- welcome.php
+++ welcome.php
@@ -81,6 +81,30 @@ for($i=0;$i<count($daysOfWeekNextSevenDays);$i++) {
     </style>
 </head>
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">IronWorks</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="welcome.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="exercise-library.php">Library</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="add-workout.php">New Workout</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="complete-workout.php">Complete Workout</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
+
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to the site.</h1>
 
     <p>
