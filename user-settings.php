defb8af00311927b2b68517af830d87ecee67dab
melvinjwilkerson <melvinjwilkerson@gmail.com>
2021-11-17 11:24:46 -0500

    Added empty user settings page.

---
+++ user-settings.php
@@ -0,0 +1,53 @@
+<?php
+// Initialize the session
+session_start();
+require_once('config.php');
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">IronWorks</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="welcome.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="exercise-library.php">Library</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="add-workout.php">New Workout</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="complete-workout.php">Complete Workout</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="user-settings.php">Settings</a>
+        </li>
+        <li class="nav-item" style="position: relative; left: 180%">
+            <a href="logout.php" class="btn btn-secondary">Log Out</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
+</body>
+</html>
