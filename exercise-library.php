fa1d8f3b9453265ce92705933ec5ce57fabce974
melvinjwilkerson <melvinjwilkerson@gmail.com>
2021-11-17 19:18:00 -0500

    Fixed active page on nav bar.

--- exercise-library.php
+++ exercise-library.php
@@ -23,10 +23,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-            <a class="nav-link" href="welcome.php">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="welcome.php">Home</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="exercise-library.php">Library</a>
+            <a class="nav-link" href="exercise-library.php">Library <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="add-workout.php">New Workout</a>
