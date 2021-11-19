eeae1afbaac16efccddfb5df2727c9b237e32ed7
melvinjwilkerson <melvinjwilkerson@gmail.com>
2021-11-18 23:25:03 -0500

    Separated frontend and backend components of the add workout page.

--- add-workout.php
+++ add-workout.php
@@ -1,16 +1,11 @@
 <?php 
 session_start();
-require_once('config.php');
+include('workout.php');
 
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
-$sql = "SELECT * FROM exercises";
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
-$allExercises = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
 <!DOCTYPE html>
@@ -59,121 +54,29 @@ $allExercises = $stmt->fetchAll(PDO::FETCH_ASSOC);
     </div>
     </nav>
     <div class="wrapper">
-        
-
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group">
                 <label>Workout Name</label>
-                <input type="text" name="workoutName" class="form-control" <?php if(isset($_POST['workoutName'])) echo "value=\"{$_POST['workoutName']}\""?> required>
+                <input type="text" name="workoutName" class="form-control" <?php displayWorkoutName(); ?> required>
             </div>
             <div class="form-group">
                 <label>Number of Exercises</label>
                 <select class="form-control" id="numExercisesSelect" name="numExercises">
-                    <option <?php if($_POST['numExercises'] == 1) echo "selected"; ?>>1</option>
-                    <option <?php if($_POST['numExercises'] == 2) echo "selected"; ?>>2</option>
-                    <option <?php if($_POST['numExercises'] == 3) echo "selected"; ?>>3</option>
-                    <option <?php if($_POST['numExercises'] == 4) echo "selected"; ?>>4</option>
-                    <option <?php if($_POST['numExercises'] == 5) echo "selected"; ?>>5</option>
-                    <option <?php if($_POST['numExercises'] == 6) echo "selected"; ?>>6</option>
-                    <option <?php if($_POST['numExercises'] == 7) echo "selected"; ?>>7</option>
-                    <option <?php if($_POST['numExercises'] == 8) echo "selected"; ?>>8</option>
-                    <option <?php if($_POST['numExercises'] == 9) echo "selected"; ?>>9</option>
-                    <option <?php if($_POST['numExercises'] == 10) echo "selected"; ?>>10</option>
+                    <?php displayExerciseCountOptions(); ?>
                 </select>
             </div>
             <div class="form-group">
                 <label>Workout Date</label>
-                <input type="date" id="workoutDate" name="workoutDate" <?php if(isset($_POST['workoutDate'])) echo "value=\"{$_POST['workoutDate']}\"" ?> required>
+                <input type="date" id="workoutDate" name="workoutDate" <?php displayWorkoutDate(); ?> required>
             </div>
             <button type="submit" class="btn btn-primary mb-2" name="submitNumExercises">Generate Exercise Fields</button>
 
         </form>
 
-        <?php 
-            if(isset($_POST['submitNumExercises'])) {
-
-                //stored in session vars because accessing them in regular variables later on doesn't seem to work
-                $_SESSION['workoutName'] = $_POST['workoutName'];
-                $_SESSION['workoutDate'] = $_POST['workoutDate'];
-                $_SESSION['numExercises'] = $_POST['numExercises'];
-                // $workoutDate = date("Y-m-d", strtotime($workoutDate));
-
-                // echo $_SESSION['workoutName'];
-                // echo $_POST['workoutDate'];
-                // echo $_POST['numExercises'];
-
-
-                for($i=1;$i<=$_POST['numExercises'];$i++) {
-                    echo "<form action=\"/fitness-app/php-fitness-app/add-workout.php\" method=\"post\">
-                    <div class=\"form-group\">
-                    <label for=\"exercise{$i}\">Exercise</label>
-                    <select class=\"form-control\" name=\"exercise{$i}\">";
-
-                    foreach($allExercises as &$exercise) {
-                        echo "<option>{$exercise['name']}</option>";
-                    }
-
-                    echo "</select>
-                    </div>";
-                }
-
-                echo "<button type=\"submit\" class=\"btn btn-primary mb-2\" name=\"submitWorkout\">Submit Workout Plan!</button>";
-                echo "</form>";
-            }
-
-            if(isset($_POST['submitWorkout'])) {
-
-                // echo $_SESSION['workoutName'];
-                // echo $workoutDate;
-                //echo $_SESSION['id'];
-                $workoutDate = $_SESSION['workoutDate'];
-                $userID = $_SESSION['id'];
-                $workoutName = $_SESSION['workoutName'];
-                $numExercises = $_SESSION['numExercises'];
-                
-                $sql2 = "INSERT INTO workouts VALUES (null, '$workoutName', $numExercises, $userID, '$workoutDate')";
-                $stmt2 = $pdo->prepare($sql2);
-
-                // echo "<br> {$_SESSION['workoutName']}";
-                // echo "<br> {$_SESSION['id']}";
-                // echo "<br> {$_SESSION['workoutDate']}";
-                
-                $stmt2->execute();
-
-                $exerciseArray = array();
-                
-                for($i=1;$i<=$numExercises;$i++) {
-                    array_push($exerciseArray, $_POST["exercise{$i}"]);
-                }
-
-                $getWorkoutID = "SELECT workoutID from workouts WHERE workoutName='$workoutName' AND userID=$userID";
-                $getWorkoutIDPrep = $pdo->prepare($getWorkoutID);
-                $getWorkoutIDPrep->execute();
-                $workoutIDarr = $getWorkoutIDPrep->fetch(PDO::FETCH_ASSOC);
-                $workoutID = $workoutIDarr['workoutID'];
-
-
-                //echo $workoutID;
-                //insert a row into workout_details for each exercise in the workout
-                foreach($exerciseArray as &$exerciseToAdd) {
-                    //get the exerciseID for the exercise we're adding to the workout_details table
-                    $getExID = "SELECT exID FROM exercises WHERE name='$exerciseToAdd'";
-                    $getExIDPrep = $pdo->prepare($getExID);
-                    $getExIDPrep->execute();
-                    $exIDarr = $getExIDPrep->fetch(PDO::FETCH_ASSOC);
-                    $exID = $exIDarr['exID'];
-                    
-                    //insert a row for the workoutID and exerciseID to workout_details table
-                    $insertExercise = "INSERT INTO workout_details VALUES ($workoutID, $exID)";
-                    $insertExercisePrep = $pdo->prepare($insertExercise);
-                    $insertExercisePrep->execute();
-                }
-            }
-
+        <?php
+            displayExerciseFields();
+            submitWorkout();
         ?>
-
-        
-        
     </div>
 </body>
 </html>
